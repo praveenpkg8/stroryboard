7b92a5a5247a2638e7095fd1f7e230c159572c3e
praveen <praveenpkg8@gmail.com>
2019-06-12 17:03:16 +0530

    configured app.yaml

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from init import init_app
+import logging
+from flask import redirect, url_for
+app = init_app(__name__)
 
-app = Flask(__name__)
 
 @app.route('/')
-def welcome():
-    return "hello world"
+def hello():
+    return redirect(url_for('authentication.profile'))
+
+
+@app.errorhandler(500)
+def server_error(e):
+    logging.exception('An error occurred during a request.')
+    return 'An internal error occurred.', 500
 
 
 if __name__ == "__main__":
