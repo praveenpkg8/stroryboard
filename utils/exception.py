c9d2a2cf02a0fcf750ecd0ef57fa28df91b804be
praveen <praveenpkg8@gmail.com>
2019-06-13 05:55:57 +0530

    created: exception handling for application

---
+++ utils/exception.py
@@ -0,0 +1,34 @@
+class EmailFormatException(Exception):
+
+    def __init__(self, value):
+        self.error_message = value
+
+
+class MobileNumberLengthException(Exception):
+
+    def __init__(self, value):
+        self.error_message = value
+
+
+class MobileNumberFormatException(Exception):
+
+    def __init__(self, value):
+        self.error_message = value
+
+
+class UserNameAlreadyTakenException(Exception):
+
+    def __init__(self, value):
+        self.error_message = value
+
+
+class UserNameLengthException(Exception):
+
+    def __init__(self, value):
+        self.error_message = value
+
+
+class AccountAlreadyExist(Exception):
+
+    def __init__(self, value):
+        self.error_message = value
