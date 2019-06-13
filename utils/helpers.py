f3f997640a4bb67c9c9d7562f34b0c98a793732c
praveen <praveenpkg8@gmail.com>
2019-06-13 05:56:43 +0530

    created: helpers function for application

---
+++ utils/helpers.py
@@ -0,0 +1,54 @@
+def construct_response_message(**kwargs):
+    return kwargs
+
+
+def dict_formation(**kwargs):
+    return kwargs
+
+
+def parse_entity(entity):
+    ent = dict_formation(
+        id=entity.key.pairs()[0][1],
+        name=entity.name.encode("utf-8"),
+        user_name=entity.user_name.encode("utf-8"),
+        mail=entity.mail.encode("utf-8"),
+        password=entity.password.encode("utf-8"),
+        mobile_number=entity.mobile_number.encode("utf-8")
+    )
+    return ent
+
+
+def parse_session(_session):
+    ses = dict_formation(
+        session_id=_session.session_id.encode("utf-8"),
+        name=_session.name.encode("utf-8"),
+        user_name=_session.user_name.encode('utf-8')
+    )
+    return ses
+
+
+builtin_list = list
+
+
+def from_datastore(entity):
+    if not entity:
+        return None
+    if isinstance(entity, builtin_list):
+        entity = entity.pop()
+
+    return entity
+
+
+def entity_list(entities, check=False):
+    _entity_list = []
+    user_name_list = []
+    mail_list = []
+    for entity in entities:
+        _entity_list.append(parse_entity(entity))
+        mail_list.append(entity.mail.encode("utf-8"))
+        user_name_list.append(entity.user_name.encode("utf-8"))
+
+    if check:
+        return user_name_list, mail_list
+
+    return _entity_list
