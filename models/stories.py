c1c8f405713450c9ef8db4737bec7fa78c0b1a1b
praveen <praveenpkg8@gmail.com>
2019-06-19 18:52:14 +0530

    added: comment model

--- models/stories.py
+++ models/stories.py
@@ -1,11 +1,11 @@
 from google.appengine.ext import ndb
 
 
-class Stories(ndb.Model):
+class Story(ndb.Model):
+    story_id = ndb.StringProperty()
     name = ndb.StringProperty()
     user_name = ndb.StringProperty()
     story = ndb.StringProperty()
-    time = ndb.TimeProperty(auto_now_add=True)
     created_on = ndb.DateTimeProperty(auto_now=True)
     updated_on = ndb.DateTimeProperty(auto_now_add=True)
 
@@ -15,6 +15,27 @@ class Stories(ndb.Model):
         return story_key
 
     @staticmethod
-    def get_all_stories():
-        stories = Stories.query().order(-Stories.created_on)
+    def get_all_story():
+        stories = Story.query().order(-Story.created_on)
         return stories.fetch()
+
+
+class Comment(ndb.Model):
+
+    comment_id = ndb.StringProperty()
+    story_id = ndb.StringProperty()
+    user_name = ndb.StringProperty()
+    name = ndb.StringProperty()
+    comment = ndb.StringProperty()
+    created_on = ndb.DateTimeProperty(auto_now=True)
+    updated_on = ndb.DateTimeProperty(auto_now_add=True)
+
+    @staticmethod
+    def create_comment(comment):
+        comment_key = comment.put()
+        return comment_key
+
+    @staticmethod
+    def get_all_comment(story_id):
+        comments = Comment.query(Comment.story_id == story_id).order(-Comment.created_on)
+        return comments.fetch()
