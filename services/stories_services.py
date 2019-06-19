f1c55798d05ffa11265280137164b19392bb1025
praveen <praveenpkg8@gmail.com>
2019-06-19 18:51:41 +0530

    added: commen services

--- services/stories_services.py
+++ services/stories_services.py
@@ -1,25 +1,51 @@
 from flask import request
+import uuid
 
-from models.stories import Stories
-from utils.helpers import parse_all_stories
+from models.stories import Story, Comment
+from utils.helpers import parse_all_story
 
 
-class StoriesServices(object):
+class StoryServices(object):
 
     @staticmethod
-    def save_stories():
+    def save_story():
+        story_id = uuid.uuid4().hex
         request_data = request.get_json()
-        story = Stories(
+        story = Story(
+            story_id=story_id,
             name=request_data.get('name'),
             user_name=request_data.get('user_name'),
             story=request_data.get('story')
         )
-        message = Stories.create_story(story)
+        message = Story.create_story(story)
         return str(message)
 
     @staticmethod
-    def retrieve_all_stories():
-        stories = Stories.get_all_stories()
-        _stories = parse_all_stories(stories)
+    def retrieve_all_story():
+        stories = Story.get_all_story()
+        _stories = parse_all_story(stories)
         return _stories
+
+
+class CommentServices(object):
+
+    @staticmethod
+    def save_comment():
+        comment_id = uuid.uuid4().hex
+        request_data = request.get_json()
+        comment = Comment(
+            comment_id=comment_id,
+            story_id=request_data.get('story_id'),
+            name=request_data.get('name'),
+            user_name=request_data.get('user_name'),
+            comment=request_data.get('comment')
+        )
+        message = Comment.create_comment(comment)
+        return message
+
+    @staticmethod
+    def retrieve_all_comment(story_id):
+        comment = Comment.get_all_comment(story_id)
+        return comment
+
 
