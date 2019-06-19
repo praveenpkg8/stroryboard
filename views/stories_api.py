d310017fb3cb249e4ee79cc90e7513d3c6680152
praveen <praveenpkg8@gmail.com>
2019-06-19 18:49:08 +0530

    added: comment post api

--- views/stories_api.py
+++ views/stories_api.py
@@ -1,17 +1,18 @@
 import json
 from flask import Blueprint
 
-from services.stories_services import StoriesServices
+from services.stories_services import StoryServices, CommentServices
 from services.user_services import UserServices
 from utils.helpers import construct_response_message
 
 story = Blueprint('profile', __name__, url_prefix='/api/story')
+comment = Blueprint('story', __name__, url_prefix='/api/comment')
 
 
 @story.route('/', methods=['POST'])
 @UserServices.check_user
 def upload_stories(user):
-    response = StoriesServices.save_stories()
+    response = StoryServices.save_story()
     message = construct_response_message(message=response)
     return json.dumps(message)
 
@@ -19,8 +20,24 @@ def upload_stories(user):
 @story.route('/', methods=['GET'])
 @UserServices.check_user
 def fetch_all_stories(user):
-    response = StoriesServices.retrieve_all_stories()
+    response = StoryServices.retrieve_all_story()
     message = construct_response_message(message=response)
+    return json.dumps(message)
+
+
+@comment.route('/', methods=['POST'])
+@UserServices.check_user
+def upload_comment(user):
+    response = CommentServices.save_comment()
+    message = construct_response_message(message=str(response))
+    return json.dumps(message)
+
+
+@comment.route('/', methods=['GET'])
+@UserServices.check_user
+def fetch_all_comment(user):
+    response = CommentServices.retrieve_all_comment()
+    message = construct_response_message(message=str(response))
     return json.dumps(message)
 
 
