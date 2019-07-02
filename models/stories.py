ff75884d22b0b9682cc5487d1e0a487ab7e2a7e8
praveen <praveenpkg8@gmail.com>
2019-07-02 21:50:28 +0530

    updated: signed url for story

--- models/stories.py
+++ models/stories.py
@@ -1,6 +1,7 @@
-import logging
 from google.appengine.ext import ndb
 from google.appengine.datastore.datastore_query import Cursor
+from google.cloud import storage
+import datetime
 
 
 class Story(ndb.Model):
@@ -8,6 +9,7 @@ class Story(ndb.Model):
     name = ndb.StringProperty()
     user_name = ndb.StringProperty()
     story = ndb.StringProperty()
+    like_count = ndb.IntegerProperty(default=0)
     created_on = ndb.DateTimeProperty(auto_now=True)
     updated_on = ndb.DateTimeProperty(auto_now_add=True)
 
@@ -20,26 +22,34 @@ class Story(ndb.Model):
     def get_all_story(cursor=None):
         _cursor = Cursor(urlsafe=cursor)
         stories = Story.query().order(-Story.created_on)
-        story, next_cursor, more = stories.fetch_page(2, start_cursor=_cursor)
-        next_cursor = next_cursor.urlsafe()
+        story, next_cursor, more = stories.fetch_page(3, start_cursor=_cursor)
+        next_cursor = next_cursor.urlsafe() if next_cursor is not None else next_cursor
         return story, next_cursor, more
 
     @staticmethod
-    def get_one_page_of_task(cursor=None):
-        query = Story.query()
-        objects, next_cursor, more = query.fetch_page(2, start_cursor=cursor)
-        fire, next_cursor, more = query.fetch_page(2, start_cursor=next_cursor)
-        # logging.info(query_iter)
-        # page = next(query_iter.pages)
-        #
-        # tasks = list(page)
-        # next_cursor = query_iter.next_page_token
+    def get_story(story_id):
+        story = Story.query(Story.story_id == story_id).get()
+        return story
 
-        return fire, next_cursor, more
+    @staticmethod
+    def update_story(story):
+        story.put()
+
+    @staticmethod
+    def file_sign(story_id):
+        storage_client = storage.Client()
+        bucket = storage_client.get_bucket("full-services.appspot.com")
+
+        blob = bucket.blob("story/" + story_id + ".jpg")
+        url = blob.generate_signed_url(
+            version='v4',
+            expiration=datetime.timedelta(minutes=30),
+            method='PUT',
+        )
+        return url
 
 
 class Comment(ndb.Model):
-
     comment_id = ndb.StringProperty()
     story_id = ndb.StringProperty()
     user_name = ndb.StringProperty()
@@ -63,3 +73,22 @@ class Comment(ndb.Model):
         return comment, next_cursor, more
 
 
+class Like(ndb.Model):
+    like_id = ndb.StringProperty()
+    story_id = ndb.StringProperty()
+    user_name = ndb.StringProperty()
+    is_active = ndb.BooleanProperty(default=True)
+
+    @staticmethod
+    def create_like(like):
+        like_key = like.put()
+        return
+
+    @staticmethod
+    def get_like(user_name, story_id):
+        like = Like.query(Like.user_name == user_name and Like.story_id == story_id).get()
+        return like
+
+    @staticmethod
+    def delete_like(like):
+        like.key.delete()
