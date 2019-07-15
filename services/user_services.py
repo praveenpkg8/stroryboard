82611305b777ccb34842a292d3e52fbce15acb9e
praveen <praveenpkg8@gmail.com>
2019-07-15 21:32:34 +0530

    modified: username and password related fields

--- services/user_services.py
+++ services/user_services.py
@@ -1,17 +1,14 @@
 import json
 import logging
 
-
 from functools import wraps
-from flask import request, redirect
-
+from flask import request
 
 from models.auth_datastore import User, Session
-from services.auth_services import fetch_all_user
+from services.auth_services import AuthServices
+
 from services.parser import Parser
-from utils.exception import UserNameAlreadyTakenException, UserNameLengthException, \
-    EmailFormatException, AccountAlreadyExist, MobileNumberFormatException, \
-    MobileNumberLengthException
+from utils.exception import EmailFormatException, AccountAlreadyExist
 from utils.helpers import from_datastore, parse_entity, parse_session, construct_response_message
 
 
@@ -25,42 +22,19 @@ class UserServices:
         @wraps(fn)
         def decorated_function(*args, **kwargs):
             request_data = request.get_json()
-            required_fields = ['name', 'user_name', 'mail', 'password', 'mobile_number']
-            username, mail = fetch_all_user(True)
+            required_fields = ['name', 'mail', 'password']
+            mail_taken = AuthServices.fetch_user_by_mail(request_data.get('mail'))
             for key in required_fields:
                 if key not in request_data:
                     pass
-
-                if key == 'user_name':
-                    try:
-                        Parser.parse_unique_user_name(request_data['user_name'], username)
-                    except UserNameAlreadyTakenException as e:
-                        message = construct_response_message(e.error_message)
-                        return json.dumps(message)
-                    except UserNameLengthException as e:
-                        message = construct_response_message(e.error_message)
-                        return json.dumps(message)
-
                 if key == 'mail':
                     try:
-                        Parser.parse_email(request_data['mail'], mail)
+                        Parser.parse_email(request_data.get('mail'), mail_taken)
                     except EmailFormatException as e:
-                        message = construct_response_message(e.error_message)
+                        message = construct_response_message(message=e.error_message)
                         return json.dumps(message)
                     except AccountAlreadyExist as e:
-                        message = construct_response_message(e.error_message)
-                        return json.dumps(message)
-
-                if key == 'mobile_number':
-                    try:
-                        Parser.parse_mobile_number(request_data['mobile_number'])
-                    except MobileNumberFormatException as e:
-
-
-                        message = construct_response_message(e.error_message)
-                        return json.dumps(message)
-                    except MobileNumberLengthException as e:
-                        message = construct_response_message(e.error_message)
+                        message = construct_response_message(message=e.error_message)
                         return json.dumps(message)
 
             return fn(request_data)
@@ -72,12 +46,13 @@ class UserServices:
         @wraps(fn)
         def decorated_function(*args, **kwargs):
             request_data = request.get_json()
-            user = parse_entity(from_datastore(User.user_by_username(request_data.get('user_name'))))
+            user = parse_entity(from_datastore(User.user_by_mail(request_data.get('mail'))))
             if user is None:
                 return fn(False)
             if user.get('password') != request_data.get('password'):
                 return fn(False)
             return fn(user)
+
         return decorated_function
 
     @staticmethod
