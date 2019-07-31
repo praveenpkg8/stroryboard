2b262a0d056caf3c810dafdc79a54b9fe8a2bdc7
praveen <praveenpkg8@gmail.com>
2019-07-31 10:25:17 +0530

    created: new token model to store model

--- models/auth_datastore.py
+++ models/auth_datastore.py
@@ -18,17 +18,35 @@ class User(ndb.Model):
         users = User.query()
         return users.fetch()
 
-
     @staticmethod
     def user_by_mail(mail):
         user = User.query(User.mail == mail).get()
         return user
 
 
+class Tokens(ndb.Model):
+    token_id = ndb.StringProperty()
+    access_token = ndb.StringProperty()
+    refresh_token = ndb.StringProperty()
+    created_on = ndb.DateTimeProperty(auto_now=True)
+    updated_on = ndb.DateTimeProperty(auto_now_add=True)
+
+    @staticmethod
+    def create_token(token):
+        token_key = token.put
+        return token_key
+
+    @staticmethod
+    def get_token(token_id):
+        token = Tokens.query(Tokens.token_id == token_id).get()
+        return token
+
+
 class Session(ndb.Model):
     session_id = ndb.StringProperty()
     mail = ndb.StringProperty()
     name = ndb.StringProperty()
+    token_id = ndb.StringProperty()
     created_on = ndb.DateTimeProperty(auto_now=True)
     updated_on = ndb.DateTimeProperty(auto_now_add=True)
 
