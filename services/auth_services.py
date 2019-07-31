41855c8236072a0ced6e89ff6ec0f9b316055b28
praveen <praveenpkg8@gmail.com>
2019-07-31 10:27:49 +0530

    modified: google authentication for creating sesson id

--- services/auth_services.py
+++ services/auth_services.py
@@ -1,9 +1,7 @@
 import uuid
-import logging
 
 from flask import session, request
 from models.auth_datastore import User, Session
-from utils.helpers import entity_list
 
 
 class AuthServices(object):
@@ -26,7 +24,7 @@ class AuthServices(object):
     @staticmethod
     def google_oauth_new_user(user_info):
         password = uuid.uuid4().hex
-        user = User.user_by_mail(user_info.get('mail'))
+        user = User.user_by_mail(user_info.get('email'))
         if user is None:
             user = User(
                 name=user_info.get('name'),
@@ -35,48 +33,40 @@ class AuthServices(object):
             )
             res = User.create_user(user)
             return "Account Created Successfully"
-        else:
-            logging.info(user)
 
     @staticmethod
-    def google_oauth_authenticate_user(user):
+    def google_oauth_authenticate_user(user, token_id):
         if user:
             session_id = str(uuid.uuid4())
             ses = Session(
                 session_id=session_id,
                 mail=user.get('email'),
+                name=user.get('name'),
+                token_id=token_id
+            )
+            res = Session.create_session(ses)
+            return session_id
+        return user
+
+    @staticmethod
+    def authenticate_user(user):
+        if user:
+            session_id = str(uuid.uuid4())
+            ses = Session(
+                session_id=session_id,
+                mail=user.get('mail'),
                 name=user.get('name')
             )
             res = Session.create_session(ses)
             return session_id
         return user
 
-
-
-def fetch_all_user():
-    users = User.get_all_user()
-    return entity_list(users)
-
-
-def authenticate_user(user):
-    if user:
-        session_id = str(uuid.uuid4())
-        ses = Session(
-            session_id=session_id,
-            mail=user.get('mail'),
-            name=user.get('name')
-        )
-        res = Session.create_session(ses)
-        logging.info(res)
-        return session_id
-    return user
-
-
-def get_user_logged_out():
-    if "session" in request.cookies:
-        session_key = Session.get_session(request.cookies.get('session'))
-        Session.delete_session(session_key)
-        return "User logged out"
+    @staticmethod
+    def get_user_logged_out():
+        if "session" in request.cookies:
+            session_key = Session.get_session(request.cookies.get('session'))
+            Session.delete_session(session_key)
+            return "User logged out"
 
 
 def get_name():
