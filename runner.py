7213df769aeef1eca3751aaa9a2ef7898e420569
praveen <praveenpkg8@gmail.com>
2019-08-13 11:24:39 +0530

    created: runner.py file to run test suite

---
+++ runner.py
@@ -0,0 +1,53 @@
+import os
+import sys
+import logging
+import unittest
+
+
+def fixup_paths(path):
+    import google
+
+    try:
+        import google
+        google.__path__.append("{0}/google".format(path))
+    except ImportError:
+        print("import error")
+
+    sys.path.insert(1, path)
+
+
+def main(sdk_path, test_path, test_pattern):
+
+    if os.path.exists(os.path.join(sdk_path, 'platform/google_appengine')):
+        sdk_path = os.path.join(sdk_path, 'platform/google_appengine')
+
+    fixup_paths(sdk_path)
+
+    import dev_appserver
+    dev_appserver.fix_sys_path()
+
+    try:
+        import appengine_config
+        (appengine_config)
+
+    except ImportError:
+        print('Issue in Importing appengine_config module')
+
+
+    print 'Discovering tests in : {}'.format(test_path)
+    suite = unittest.TestLoader().discover(test_path, test_pattern)
+    result = unittest.TextTestRunner(verbosity=2).run(suite)
+    return result
+
+
+if __name__ == '__main__':
+
+    sdk_path = '/Users/user/google-cloud-sdk'
+    test_path = os.path.dirname(os.path.abspath(__name__)) + '/test'
+    test_pattern = 'test_*'
+
+    result = main(sdk_path, test_path, test_pattern)
+
+    if not result.wasSuccessful():
+        sys.exit(1)
+
