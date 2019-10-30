956d107806c5f5652808e307d2df043c17a5cf01
praveen <praveenpkg8@gmail.com>
2019-10-30 21:18:18 +0530

    created a script

---
+++ startup.sh
@@ -0,0 +1,18 @@
+#Install gcloud to system
+
+curl https://sdk.cloud.google.com | bash
+exec -l $SHELL
+
+gcloud_path=/home/runner/google-cloud-sdk/
+echo $gcloud_path
+
+#install python and pip
+apt-get update -y
+apt-get -y install python2.7 python-pip
+
+
+#installing requirements
+pip --no-cache-dir install -r requirememts.txt
+
+#run test 
+python runner.py
